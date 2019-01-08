87c304a51f2be688c30a7c8cfd8da187cd9c27a6
maretes <kolom100@gmail.com>
2019-01-08 21:12:57 +0200

    hw_11 index

---
+++ Ivan_Kolomiiets_Homework/homework_11/js/popup.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    PopUpHide();
+});
+
+function PopUpShow() {
+    $("#popup1 ").show();
+}
+
+function PopUpHide() {
+    $("#popup1 ").hide();
+}
+
+$(document).ready(function () {
+    PopUpHide2();
+});
+
+function PopUpShow2() {
+    $("#popup2").show();
+}
+
+function PopUpHide2() {
+    $("#popup2").hide();
+}
+
+$(document).ready(function () {
+    PopUpHide3();
+});
+
+function PopUpShow3() {
+    $("#popup3").show();
+}
+
+function PopUpHide3() {
+    $("#popup3").hide();
+}
+
+$(document).ready(function () {
+    PopUpHide4();
+});
+
+function PopUpShow4() {
+    $("#popup4 ").show();
+}
+
+function PopUpHide4() {
+    $("#popup4 ").hide();
+}
+$(document).ready(function () {
+    PopUpHide5();
+});
+
+function PopUpShow5() {
+    $("#popup5 ").show();
+}
+
+function PopUpHide5() {
+    $("#popup5 ").hide();
+}
+$(document).ready(function () {
+    PopUpHide6();
+});
+
+function PopUpShow6() {
+    $("#popup6 ").show();
+}
+
+function PopUpHide6() {
+    $("#popup6 ").hide();
+}
